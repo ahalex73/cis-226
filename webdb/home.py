7cda73dae340bde54eae5dad4239fbeff283a130
Alexander Holmes <ahalex73@yahoo.com>
2022-11-07 17:28:47 -0500

    Adds Price per pound and Total Price functionality

--- webdb/home.py
+++ webdb/home.py
@@ -9,20 +9,58 @@ app.secret_key = b'Upg3zhGacjrP4mAf6bDU71GL7p7acQvPHIQknxBte4o'
 # Our DB class does not need to know anything about flask or the web
 class Vegetables:
     def __init__(self, conn):
+        """Initializes Vegetable"""
         self.conn = conn
         self.c = self.conn.cursor()
+
     def setup(self):
-        self.c.execute("CREATE TABLE IF NOT EXISTS vegetable (quantity INTEGER, name TEXT)")
+        """Sets up the vegetable"""
+        self.c.execute(
+            'CREATE TABLE IF NOT EXISTS vegetable ('
+            'pk INTEGER NOT NULL PRIMARY KEY,'
+            'quantity INTEGER,'
+            'name TEXT,'
+            'price DECIMAL,'
+            'total_price DECIMAL)'
+                )
         self.conn.commit()
     def get_all(self):
+        """Gather all"""
         for row in self.c.execute("SELECT * FROM vegetable"):
             yield row
-    def add_vegetable(self, name, quantity):
-        self.c.execute("INSERT INTO vegetable VALUES (?, ?)", [quantity, name])
+
+    def add_vegetable(self, name, quantity, price, total_price, pk):
+        """Adds an order to the vegetable table"""
+        total_price = (float(quantity) * float(price))
+        if pk:
+            # Do an UPDATE
+            self.c.execute(
+                "UPDATE vegetable SET name=?, quantity=?, price=?, total_price WHERE pk=?",
+                [name, quantity, price, total_price, pk])
+        else:
+            # Do an INSERT
+            self.c.execute(
+                "INSERT INTO vegetable (name, quantity, price, total_price) VALUES (?, ?, ?, ?)",
+                [name, quantity, price, total_price])
+
+            # Get a pk for the insert
+            pk = self.c.lastrowid
+        
+
+        self.conn.commit()
+
     def find_vegetable(self, name):
         self.c.execute("SELECT * FROM vegetable WHERE name=?", [name])
         row = self.c.fetchone() # Get first row
         return row
+
+    # def get_total_price(self, quantity, price, pk):
+    #     """Gets the total price for each item"""
+    #     self.c.execute("SELECT quantity, price WHERE pk=?", [quantity, price, pk])
+    #     total_price = quantity * price
+    #     self.c.execute("INSERT INTO vegetable (total_price) VALUES (?)", [total_price])
+        
+    #     self.conn.commit()
 
 def db_setup():
     """Setup the db if needed"""
@@ -37,6 +75,9 @@ app.before_first_request(db_setup)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    pk = 0
+    price = ''
+    total_price = ''
     name = ''
     quantity = ''
     valid = False
@@ -44,6 +85,7 @@ def index():
         # Validate form
         name = request.form.get('name')
         quantity = request.form.get('quantity')
+        price = request.form.get('price')
         valid = True
         if not name or not quantity:
             flash('You must give a name and a quantity')
@@ -58,10 +100,10 @@ def index():
     # Open and close the db within the request
     with sqlite3.connect(DB_PATH) as conn:
         v = Vegetables(conn)
-        # Add vegetable if form was valid
+        # Add order if form was valid
         if valid:
-            v.add_vegetable(name, quantity)
-            flash('Vegetable {} was added with quantity {}'.format(name, quantity))
+            v.add_vegetable(name, quantity, price, total_price, pk)
+            flash('{} was added with quantity {}'.format(name, quantity))
             # Always redirect after a POST was valid
             return redirect(url_for('index'))
         # Get new list of vegetables to display
@@ -72,4 +114,6 @@ def index():
         vegetables=vegetables,
         name=name,
         quantity=quantity,
+        price=price,
+        total_price=total_price,
     )
